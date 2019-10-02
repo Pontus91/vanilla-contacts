57ad2c86cc78161cda8637b5691bfdec85fefd91
unknown <p0nta_@hotmail.com>
2019-10-02 20:54:30 +0200

    Added more functionality on adding phonenummbers to instance object, also added more CSS

--- Layout.js
+++ Layout.js
@@ -17,7 +17,7 @@ wrapper.setAttribute('class', 'wrapper');
  * Creating a simple heading.
  */
 const heading = document.createElement('h1');
-heading.innerHTML = 'Mina Kontakter';
+heading.innerHTML = 'Min Kontaktlista';
 heading.setAttribute('class', 'title');
 
 /**
@@ -40,6 +40,9 @@ const emailLabel = document.createElement('label');
 const nameInput = document.createElement('input');
 const phoneInput = document.createElement('input');
 const emailInput = document.createElement('input');
+const addNumber = document.createElement('button');
+const holder = document.createElement('div');
+const phoneText = document.createElement('div');
 
 /**
  * Creating a Button
@@ -47,6 +50,7 @@ const emailInput = document.createElement('input');
 const Button = document.createElement('Button');
 Button.setAttribute('class', 'Button');
 Button.innerHTML = 'Skapa kontakt';
+addNumber.innerHTML = 'Lägg till fler nummer';
 
 /**
  * Attributes to form, input and labels
@@ -55,12 +59,12 @@ form.setAttribute('class', 'form');
 nameDiv.setAttribute('class', 'nameDiv');
 nameLabel.setAttribute('class', 'nameLabel');
 nameInput.setAttribute('class', 'nameInput');
-phoneDiv.setAttribute('class', 'nameDiv');
-phoneLabel.setAttribute('class', 'nameLabel');
-phoneInput.setAttribute('class', 'nameInput');
-emailDiv.setAttribute('class', 'nameDiv');
-emailLabel.setAttribute('class', 'nameLabel');
-emailInput.setAttribute('class', 'nameInput');
+phoneDiv.setAttribute('class', 'phoneDiv');
+phoneLabel.setAttribute('class', 'phoneLabel');
+phoneInput.setAttribute('class', 'phoneInput');
+emailDiv.setAttribute('class', 'emailDiv');
+emailLabel.setAttribute('class', 'emailLabel');
+emailInput.setAttribute('class', 'emailInput');
 
 /**
  * Elements for the contacts
@@ -88,12 +92,15 @@ wrapper.appendChild(ContactWrapper);
 ContactWrapper.appendChild(ul);
 InputWrapper.appendChild(nameDiv);
 InputWrapper.appendChild(phoneDiv);
+InputWrapper.appendChild(phoneText);
 InputWrapper.appendChild(emailDiv);
 InputWrapper.appendChild(Button);
 nameDiv.appendChild(nameLabel);
 nameDiv.appendChild(nameInput);
 phoneDiv.appendChild(phoneLabel);
-phoneDiv.appendChild(phoneInput);
+phoneDiv.appendChild(holder);
+holder.appendChild(phoneInput);
+holder.appendChild(addNumber);
 emailDiv.appendChild(emailLabel);
 emailDiv.appendChild(emailInput);
 body.append(container);
@@ -102,21 +109,12 @@ body.append(container);
 /**
  * Attributes for the input and button.
  */
-Button.setAttribute('onclick', '');
-nameInput.setAttribute('name', 'test');
-nameInput.setAttribute('id', 'name')
+nameInput.setAttribute('id', 'nameValue');
+phoneInput.setAttribute('id', 'phoneValue');
+phoneInput.setAttribute('list', 'phoneNumbers');
+emailInput.setAttribute('id', 'emailValue');
+phoneInput.setAttribute('type', 'text');
+addNumber.setAttribute('class', 'add');
+holder.setAttribute('class', 'holder');
+phoneText.setAttribute('class', 'phoneText');
 
-class Contact {
-
-  constructor(name, phoneNumber) {
-    this.name = name;
-    this.phoneNumber = phoneNumber;
-  }
-
-}
-
-document.querySelector('Button').addEventListener("click", function () {
-  let nameValue = document.querySelector('#name').value;
-  let testGuy = new Contact(nameValue, ['010-101010', '0735-033-033']);
-  console.log(testGuy)
-})
